591948fe5343ae8af3eefd557a3a1b5ba74f4ca8
DESKTOP-7NDSE35\tpfla <tpfla311@naver.com>
2023-12-02 19:53:12 +0900

    commit

--- main.js
+++ main.js
@@ -4,17 +4,28 @@ const menus = document.querySelectorAll(".menus button");
 console.log(menus);
 menus.forEach(menu => menu.addEventListener("click",(event)=>getNewsByCategory(event)));
 let url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
+let totalResults = 0;
+let page = 1;
+const pageSize = 10;
+const groupSize = 5;
+
 
 const getNews = async() =>{
-    try {
+    try {      
+        url.searchParams.set("page",page); //-> &page=page
+        url.searchParams.set("pageSize",pageSize);
+
         const response = await fetch(url);
+
         const data = await response.json();
         if(response.status===200){
             if(data.articles.length === 0 ){
                 throw new Error("No result for this search");
             }
             newsList = data.articles;
+            totalResults = data.totalResults;
             render();
+            paginationRender();
         }else{
             throw new Error(data.message);
         }
@@ -65,6 +76,39 @@ const errorRender = (errorMessage) => {
     const errorHTML=`<div class="alert alert-danger" role="alert">
    ${errorMessage} </div>`;
    document.getElementById("news-board").innerHTML = errorHTML;
+}
+
+const paginationRender = () => {
+    //totalResult
+    //page
+    //pageSize
+    //groupSize
+    //totalPage
+    const totalPages = Math.ceil(totalResults / pageSize);
+    //pageGroup
+    const pageGroup = Math.ceil(page/groupSize);
+    //lastPage
+    let lastPage = pageGroup * groupSize;
+    if(lastPage > totalPages){
+        lastPage = totalPages;
+    }
+    //firstPage
+    const firstPage = lastPage - (groupSize -1) <=0 ? 1:lastPage - (groupSize -1);
+
+    let paginationHTML = `<li class="page-item" onclick="moveToPage(${page-1})"><a class="page-link" href="#">Previous</a></li>`;
+
+    for(let i = firstPage;i<=lastPage;i++){
+        paginationHTML += `<li class="page-item ${i===page?"active":""}" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`;
+    }
+
+    paginationHTML += `<li class="page-item" onclick="moveToPage(${page+1})"><a class="page-link" href="#">Next</a></li>`;
+    document.querySelector(".pagination").innerHTML = paginationHTML;
+}
+
+const moveToPage = (pageNum) => {
+    console.log("click",pageNum);
+    page = pageNum;
+    getNews();
 }
 
 getLatesNews();
